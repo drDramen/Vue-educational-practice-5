b2dfc671fc1ec03bc59af8427a92b1e87828ba73
drDramen <vvpalchin@gmail.com>
2021-10-26 09:46:51 +0300

    save

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table-list v-bind="{ fields, items: users }">
+      <template #table-header="{ field }">
+        {{ field }}
+      </template>
+      <template #table-body="{ item: user, myKey: key, field }">
+        <div :key="key" class="table__cell">{{ user[field] }}</div>
+      </template>
+    </table-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
+import TableList from '@/components/TableList.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      fields: ['username', 'name', 'email', 'phone'],
+      users: [],
+    };
   },
+  created() {
+    this.getUsers();
+  },
+  methods: {
+    async getUsers() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users?_limit=5');
+      this.users = await response.json();
+    },
+  },
+  components: { TableList },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  max-width: 1200px
+  margin: 0 auto
+  padding: 50px 30px 0
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+
 </style>
---
+++ src/components/TableList.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="table">
+    <template v-for="(field, idx) in fields">
+      <div :key="idx" class="table__column">
+        <div class="table__header">
+          <div class="table__cell">
+            <slot
+              name="table-header"
+              :field="field"
+            >
+            </slot>
+          </div>
+
+        </div>
+        <div class="table__body">
+          <slot
+            name="table-body"
+            v-for="item in items"
+            :myKey="item.id"
+            :item="item"
+            :field="field"
+          >
+          </slot>
+        </div>
+      </div>
+    </template>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableList',
+  props: {
+    fields: { type: Array, required: true },
+    items: Array,
+  },
+};
+</script>
+
+<style lang="sass">
+.table
+  display: flex
+  justify-content: space-between
+  text-align: center
+  padding: 20px
+  border: 1px solid #53e3b0
+
+  &__column
+    flex-direction: column
+    flex: 1 0 0
+
+    & + &
+      .table__cell
+        border-left: none
+
+    .table__cell + .table__cell
+      border-top: none
+
+  &__header &__cell
+    text-transform: uppercase
+    font-weight: bold
+
+  &__cell
+    padding: 5px 10px
+    border: 1px solid #000
+
+</style>
